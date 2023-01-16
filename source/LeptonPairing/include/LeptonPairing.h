ae53a0ebd98f9665fffaa426a6f1ab4258fdeef8
Julie Munch Torndal <julie.munch.torndal@desy.de>
2023-01-16 21:03:53 +0100

    LeptonPairing: include package

---
+++ source/LeptonPairing/include/LeptonPairing.h
@@ -0,0 +1,62 @@
+#ifndef LeptonPairing_h
+#define LeptonPairing_h 1
+
+#include "marlin/Processor.h"
+#include "IMPL/LCCollectionVec.h"
+#include "lcio.h"
+#include <string>
+#include <vector>
+#include "TLorentzVector.h"
+#include "EVENT/LCCollection.h"
+#include "EVENT/ReconstructedParticle.h"
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/ReconstructedParticleImpl.h"
+#include "IMPL/ParticleIDImpl.h"
+#include "UTIL/PIDHandler.h"
+#include "marlin/VerbosityLevels.h"
+#include "Utilities.h"
+#include "TFile.h"
+#include "TH1I.h"
+#include "TTree.h"
+
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std;
+
+class LeptonPairing : public Processor 
+{
+ public: 
+  virtual Processor*  newProcessor() {
+    return new LeptonPairing;
+  }
+  LeptonPairing();
+  virtual ~LeptonPairing() = default;
+  LeptonPairing(const LeptonPairing&) = delete;
+  LeptonPairing& operator=(const LeptonPairing&) = delete;
+  virtual void init();
+  virtual void Clear();
+  virtual void processRunHeader(LCRunHeader* run);
+  virtual void processEvent( EVENT::LCEvent *pLCEvent );
+  virtual void check(EVENT::LCEvent *pLCEvent);
+  virtual void end();
+
+ protected:
+  double m_diLepInvMass = 91.2;
+  string m_rootFile{};
+  bool m_fillRootTree = false;
+  int m_nRun;
+  int m_nEvt;
+  vector<float> m_IsoLepsInvMass;
+  vector<float> m_RecoLepsInvMass;
+  TFile *m_pTFile{};
+  TTree *m_pTTree{};
+
+  string m_inputIsoLepCollection{};
+  string m_inputPFOsWOIsoLepCollection{};
+  string m_outputLepPairCollection{};
+  string m_outputPFOsWOLepPairCollection{};
+  
+
+};
+#endif
---
+++ source/LeptonPairing/src/LeptonPairing.cc
@@ -0,0 +1,262 @@
+#include "LeptonPairing.h"
+#include <stdlib.h>
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include "Utilities.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "TTree.h"
+
+#include "marlin/VerbosityLevels.h"
+
+#ifdef MARLIN_USE_AIDA
+#include <marlin/AIDAProcessor.h>
+#include <AIDA/IHistogramFactory.h>
+#include <AIDA/ICloud1D.h>
+//#include <AIDA/IHistogram1D.h>                                                                                                                              
+#endif // MARLIN_USE_AIDA  
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std ;
+
+using namespace isolep;
+
+template<class T>
+double inv_mass(T* p1, T* p2){
+  double e = p1->getEnergy()+p2->getEnergy() ;
+  double px = p1->getMomentum()[0]+p2->getMomentum()[0];
+  double py = p1->getMomentum()[1]+p2->getMomentum()[1];
+  double pz = p1->getMomentum()[2]+p2->getMomentum()[2];
+  return( sqrt( e*e - px*px - py*py - pz*pz  ) );
+}
+
+template<class T>
+TLorentzVector v4(T* p){
+  return TLorentzVector( p->getMomentum()[0],p->getMomentum()[1], p->getMomentum()[2],p->getEnergy());
+}
+
+LeptonPairing aLeptonPairing;
+
+LeptonPairing::LeptonPairing():
+  Processor("LeptonPairing"),
+  m_nRun(0),
+  m_nEvt(0),
+  m_IsoLepsInvMass{},
+  m_RecoLepsInvMass{}
+
+{
+  _description = "LeptonPairing pairs isolated leptons of opposite charge and adds additional leptons to the list of PFOs or rejects the event" ;
+
+  registerInputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+			  "IsoLepCollection",
+			  "Name of input Isolated Lepton collection",
+			  m_inputIsoLepCollection,
+			  string("ISOLeptons")
+			  );
+  
+  registerInputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+			  "PFOsWOIsoLepCollection",
+			  "Name of input PFO collection without isolated leptons",
+			  m_inputPFOsWOIsoLepCollection,
+			  string("PandoraPFOsWithoutIsoLep")
+			  );
+
+  registerOutputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+			   "LepPairCollection",
+			   "Name of output lepton pair collection",
+			   m_outputLepPairCollection,
+			   string("LeptonPair")
+			   );
+
+  registerOutputCollection(LCIO::RECONSTRUCTEDPARTICLE,
+			   "PFOsWOLepPairCollection",
+			   "Name of output PFO collection without lepton pair",
+			   m_outputPFOsWOLepPairCollection,
+			   string("PandoraPFOsWithoutLepPair")
+			   );
+
+  registerProcessorParameter("diLepInvMass",
+			     "Invariant mass of di-lepton system in Isolated Leptons [GeV]",
+			     m_diLepInvMass,
+			     double(91.2)
+			     );
+
+  registerProcessorParameter(     "fillRootTree",
+				  "Fill root tree to check processor performance",
+				  m_fillRootTree,
+				  bool(true)
+				  );
+
+  registerProcessorParameter(     "RootFile",
+				  "Name of the output root file",
+				  m_rootFile,
+				  string("Output.root")
+				  );
+}
+
+void LeptonPairing::init() {
+  streamlog_out(DEBUG0) << "   init called  " << endl ;
+  printParameters();
+  if ( m_fillRootTree ) {
+    streamlog_out(DEBUG0) << "      Creating root file/tree/histograms" << endl ;
+    m_pTFile = new TFile(m_rootFile.c_str(), "recreate");
+    m_pTTree = new TTree("LeptonPairing", "LeptonPairing");
+    m_pTTree->SetDirectory(m_pTFile);
+    m_pTTree->Branch("event", &m_nEvt, "event/I");
+    m_pTTree->Branch("IsoLepsInvMass", &m_IsoLepsInvMass);
+    m_pTTree->Branch("RecoLepsInvMass", &m_RecoLepsInvMass);
+    streamlog_out(DEBUG0) << "      Created root file/tree/histograms" << endl ;
+  }
+
+  this->Clear();
+  m_nRun = 0;
+  m_nEvt = 0;
+  streamlog_out(DEBUG) << "   init finished  " << endl;
+}
+
+void LeptonPairing::Clear() {
+  streamlog_out(DEBUG) << "   Clear called  " << endl;
+  m_IsoLepsInvMass.clear();
+  m_RecoLepsInvMass.clear();
+}
+
+void LeptonPairing::processRunHeader( LCRunHeader* run ) { 
+
+} 
+
+void LeptonPairing::processEvent( EVENT::LCEvent *pLCEvent ) {
+  streamlog_out(DEBUG0) << "      <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       processEvent Called     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl ;
+  m_nRun = pLCEvent->getRunNumber();
+  m_nEvt = pLCEvent->getEventNumber();
+  streamlog_out(DEBUG4) << "" << endl;
+  streamlog_out(DEBUG4) << "      ////////////////////////////////////////////////////////////////////////////" << endl;
+  streamlog_out(DEBUG4) << "      ////////////////////    Processing event        " << m_nEvt << "        ////////////////////" << endl;
+  streamlog_out(DEBUG4) << "      ////////////////////////////////////////////////////////////////////////////" << endl;
+  this->Clear();
+
+  EVENT::LCCollection *IsoLepCollection{};
+  EVENT::LCCollection *PFOsWOIsoLepCollection{};
+  try {
+    IsoLepCollection = pLCEvent->getCollection(m_inputIsoLepCollection);
+    PFOsWOIsoLepCollection = pLCEvent->getCollection(m_inputPFOsWOIsoLepCollection);
+    streamlog_out(DEBUG7) << "Input collections successfully found in event " << m_nEvt << endl; 
+  }
+  catch( DataNotAvailableException &e ) {
+    streamlog_out(MESSAGE) << "     Input collection not found in event " << m_nEvt << endl;
+  }
+  IMPL::LCCollectionVec* m_LepPairCol = new IMPL::LCCollectionVec( LCIO::RECONSTRUCTEDPARTICLE );
+  //m_LepPairCol->setSubset( true );
+  IMPL::LCCollectionVec* m_PFOsWOLepPairCol = new IMPL::LCCollectionVec( LCIO::RECONSTRUCTEDPARTICLE );
+  m_PFOsWOLepPairCol->setSubset( true );
+
+  int InIsoLeps = IsoLepCollection->getNumberOfElements();
+  streamlog_out(DEBUG7) << "Number of iso leptons = " << InIsoLeps << endl;
+  vector< ReconstructedParticle* > LeptonPair = {};
+  int _lep_type = 0;
+  Double_t fCosFSRCut = 0.99;   // the angle of BS and FSR around the direction of charged lepton
+  if (InIsoLeps == 2) {
+    ReconstructedParticle* lepton1 = static_cast<ReconstructedParticle*>( IsoLepCollection->getElementAt( 0 ) );
+    ReconstructedParticle* lepton2 = static_cast<ReconstructedParticle*>( IsoLepCollection->getElementAt( 1 ) );
+    //Check if same type and have opposite charge
+    streamlog_out(DEBUG7) << "pre check" << endl;
+    streamlog_out(DEBUG7) << lepton1->getType() << " " << lepton2->getType()<< " " << lepton1->getCharge() << " " << lepton2->getCharge() << endl;
+    if (lepton1->getType() + lepton2->getType() == 0) {
+      LeptonPair = {lepton1, lepton2};
+      _lep_type = lepton1->getType();
+      streamlog_out(DEBUG7) << "mass = " << inv_mass(lepton1, lepton2)  << endl;
+      streamlog_out(DEBUG7) << "same type, opposite charge" << endl;
+    }
+    streamlog_out(DEBUG7) << "post check" << endl;
+  } else if (InIsoLeps > 2) {
+    float mindelta = 99999.;
+    for ( int i_lep1 = 0 ; i_lep1 < InIsoLeps - 1 ; ++i_lep1 ) {
+      ReconstructedParticle* lepton1 = static_cast<ReconstructedParticle*>( IsoLepCollection->getElementAt( i_lep1 ) );
+      for ( int i_lep2 = i_lep1 + 1 ; i_lep2 < InIsoLeps ; ++i_lep2 ) {
+	ReconstructedParticle* lepton2 = static_cast<ReconstructedParticle*>( IsoLepCollection->getElementAt( i_lep2 ) );
+	//Check if same type and have opposite charge 
+	if (lepton1->getType() + lepton2->getType() == 0) {
+	  float pairmass = inv_mass(lepton1, lepton2);
+	  streamlog_out(DEBUG7) << "mass = " << pairmass  << endl;
+	  float delta = abs(pairmass-m_diLepInvMass);
+	  if (delta > mindelta) continue;
+	  mindelta = delta;  
+	  LeptonPair = {lepton1, lepton2};
+	  _lep_type = lepton1->getType();
+	} 
+      }
+    }
+  }
+  //Select lepton pair
+  //(add pre invmass to root tree)
+  //Do FSR and BS recovery
+  //(add post invmass to root tree)
+  //Add reco lepton pair to output collection
+  //Add all other pfos (including additional leptons) that are not from lepton pair or photons to output
+
+  vector<lcio::ReconstructedParticle*> photons;
+  if (LeptonPair.size() == 2) {
+    // recovery of FSR and BS
+    m_IsoLepsInvMass.push_back(inv_mass(LeptonPair[0], LeptonPair[1]));
+    ReconstructedParticleImpl * recoLepton1 = new ReconstructedParticleImpl();
+    streamlog_out(DEBUG7) << "pre recovery" << endl;
+    streamlog_out(DEBUG7) << "lep" <<LeptonPair.at(0) << endl;
+    streamlog_out(DEBUG7) << "pfos" << PFOsWOIsoLepCollection << endl;
+    streamlog_out(DEBUG7) << "recolep" << recoLepton1 << endl;
+    streamlog_out(DEBUG7) << "cut" << fCosFSRCut << endl;
+    streamlog_out(DEBUG7) << "type" << _lep_type << endl;
+    //streamlog_out(DEBUG7) << "photons"<< photons << endl;
+    doPhotonRecovery(LeptonPair[0],PFOsWOIsoLepCollection,recoLepton1,fCosFSRCut,_lep_type,photons);
+    ReconstructedParticleImpl * recoLepton2 = new ReconstructedParticleImpl();
+    doPhotonRecovery(LeptonPair[1],PFOsWOIsoLepCollection,recoLepton2,fCosFSRCut,_lep_type,photons);
+    streamlog_out(DEBUG7) << "post recovery" << endl;    
+    streamlog_out(DEBUG7) << "mass =" << inv_mass(recoLepton1, recoLepton2)  << endl;
+    m_RecoLepsInvMass.push_back(inv_mass(recoLepton1,recoLepton2));
+    if(recoLepton1) streamlog_out(DEBUG7) << "not null" << endl;
+    if(recoLepton2) streamlog_out(DEBUG7) << "not null"<< endl;
+    streamlog_out(DEBUG7) << recoLepton1->getEnergy() << " " << recoLepton1->getType() << endl;
+    streamlog_out(DEBUG7) << recoLepton2->getEnergy() << " " << recoLepton2->getType() << endl;
+    m_LepPairCol->addElement(recoLepton1);
+    m_LepPairCol->addElement(recoLepton2);
+  }
+  for ( int i_lep = 0 ; i_lep < InIsoLeps ; ++i_lep ) {
+    bool isFromPair = false;
+    ReconstructedParticle* lepton = static_cast<ReconstructedParticle*>(IsoLepCollection->getElementAt(i_lep));
+    for (auto leptonfrompair: LeptonPair) {
+      if (lepton == leptonfrompair) isFromPair = true;
+    }
+    if (!isFromPair) m_PFOsWOLepPairCol->addElement(lepton);
+  }
+  for (int i=0; i<PFOsWOIsoLepCollection->getNumberOfElements(); i++) {
+    bool isPhoton = false;
+    ReconstructedParticle* pfo =static_cast<ReconstructedParticle*>(PFOsWOIsoLepCollection->getElementAt(i));
+    for (auto photon: photons) {
+      if (pfo == photon) isPhoton = true;
+    }
+    if (!isPhoton)  m_PFOsWOLepPairCol->addElement(pfo);
+  }
+  streamlog_out(DEBUG7) << "nphotons " << photons.size() << endl;
+  streamlog_out(DEBUG7) << "nselected " << m_LepPairCol->getNumberOfElements() << endl;
+  streamlog_out(DEBUG7) << "npfos " << PFOsWOIsoLepCollection->getNumberOfElements() << endl;
+  streamlog_out(DEBUG7) << "nnewpfos " << m_PFOsWOLepPairCol->getNumberOfElements() << endl;
+  pLCEvent->addCollection(m_LepPairCol, m_outputLepPairCollection.c_str());
+  pLCEvent->addCollection(m_PFOsWOLepPairCol, m_outputPFOsWOLepPairCollection.c_str());
+  if ( m_fillRootTree ) m_pTTree->Fill();
+}
+
+void LeptonPairing::check(EVENT::LCEvent *pLCEvent) {
+  // nothing to check here - could be used to fill checkplots in reconstruction processor
+}
+
+
+void LeptonPairing::end() {
+  if ( m_fillRootTree ) {
+    m_pTFile->cd();
+    m_pTTree->Write();
+    m_pTFile->Close();
+    delete m_pTFile;
+  }
+
+}
