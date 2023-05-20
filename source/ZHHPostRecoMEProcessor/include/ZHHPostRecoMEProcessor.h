4e5a1295cc8f508ba58dfa43c4f0f553e7f9a6cf
Bryan Bliewert <bryan.bliewert@nventis.eu>
2023-05-20 14:59:04 +0200

    Output 4-vectors of all truth and reco particles

--- source/ZHHPostRecoMEProcessor/include/ZHHPostRecoMEProcessor.h
+++ source/ZHHPostRecoMEProcessor/include/ZHHPostRecoMEProcessor.h
@@ -1,3 +1,9 @@
+/**
+ * @author Bryan Bliewert (TUM/DESY) <bryan.bliewert@tum.de>
+ * @date 20.05.2023
+ * 
+*/
+
 #ifndef ZHHPostRecoMEProcessor_h
 #define ZHHPostRecoMEProcessor_h 1
 
@@ -52,6 +58,7 @@ class ZHHPostRecoMEProcessor : public Processor
 		std::string m_inputPreSelectionCollection{};
 		std::string m_inputMCTrueCollection{};
 		std::string m_outputFile{};
+		std::string m_outputTree{};
 
 		int m_nRun;
         int m_nEvt;
@@ -64,39 +71,92 @@ class ZHHPostRecoMEProcessor : public Processor
 
 		// Refreshed every run
 		int m_passed_preselection{};
+		int m_passed_validitycheck{};
 		int m_true_h1_decay_pdg{};
 		int m_true_h2_decay_pdg{};
 
+		// 1. True
+		// 1.a ZHH output
 		float m_true_sigma{};
 		float m_true_sigmall{};
 		float m_true_sigmalr{};
 		float m_true_sigmarl{};
 		float m_true_sigmarr{};
+
 		float m_true_mz{};
 		float m_true_mhh{};
 		float m_true_mzhh{};
+
 		float m_true_phi{};
 		float m_true_phif{};
 		float m_true_phih{};
+
 		float m_true_costheta{};
 		float m_true_costhetaf{};
 		float m_true_costhetah{};
+
+		// 1.b ZHH input
+		float m_true_l1_E{};
+  		float m_true_l1_px{};
+  		float m_true_l1_py{};
+  		float m_true_l1_pz{};
 		
+		float m_true_l2_E{};
+  		float m_true_l2_px{};
+  		float m_true_l2_py{};
+  		float m_true_l2_pz{};
+
+		float m_true_h1_E{};
+  		float m_true_h1_px{};
+  		float m_true_h1_py{};
+  		float m_true_h1_pz{};
+
+		float m_true_h2_E{};
+  		float m_true_h2_px{};
+  		float m_true_h2_py{};
+  		float m_true_h2_pz{};
+
+
+		// 2. Reco
+		// 2.a ZHH output
 		float m_reco_sigma{};
 		float m_reco_sigmall{};
 		float m_reco_sigmalr{};
 		float m_reco_sigmarl{};
 		float m_reco_sigmarr{};
+
 		float m_reco_mz{};
 		float m_reco_mhh{};
 		float m_reco_mzhh{};
+
 		float m_reco_phi{};
 		float m_reco_phif{};
 		float m_reco_phih{};
+
 		float m_reco_costheta{};
 		float m_reco_costhetaf{};
 		float m_reco_costhetah{};
 
+		// 2.b ZHH input
+		float m_reco_l1_E{};
+  		float m_reco_l1_px{};
+  		float m_reco_l1_py{};
+  		float m_reco_l1_pz{};
+		
+		float m_reco_l2_E{};
+  		float m_reco_l2_px{};
+  		float m_reco_l2_py{};
+  		float m_reco_l2_pz{};
+
+		float m_reco_h1_E{};
+  		float m_reco_h1_px{};
+  		float m_reco_h1_py{};
+  		float m_reco_h1_pz{};
+
+		float m_reco_h2_E{};
+  		float m_reco_h2_px{};
+  		float m_reco_h2_py{};
+  		float m_reco_h2_pz{};
 };
 
 #endif
