Fixed normalization Added root summary output dtype Change via the dtype=root/numpy flag

workflows/analysis/tasks.py
@@ -157,6 +157,8 @@ class AnalysisSummary(BaseTask, HTCondorWorkflow):
         return self.local_target(f'{self.branch}_Presel.{"npy" if self.dtype == "numpy" else "root"}')
 
     def run(self):
+        from zhh import numpy2root
+        
         src = self.branch_map[self.branch]
         DATA_ROOT, branches, analysis_chunks, processes_index = src
         
@@ -171,7 +173,10 @@ class AnalysisSummary(BaseTask, HTCondorWorkflow):
         presel_result = analysis_stack(DATA_ROOT, processes, chunks_factual, branches,
                                      kinematics=True, b_tagging=True, final_states=True)
         
-        np.save(output.path, presel_result)
+        if self.dtype == 'numpy':
+            np.save(output.path, presel_result)
+        else:
+            numpy2root(presel_result, output.path, 'summary')
         
         self.publish_message(f'Processed {len(branches)} branches')
 

zhh/analysis/Normalization.py
@@ -54,12 +54,9 @@ def get_process_normalization(
     if RATIO_BY_TOTAL is None:
         results['n_events_target'] = results['n_events_tot']
     else:
-        results['n_events_target'] = np.maximum(
-            np.minimum(results['n_events_tot'], np.ceil(RATIO_BY_TOTAL * results['n_events_tot'])),
-            50*np.ones(len(results), dtype=int)
-        )
+        results['n_events_target'] = np.minimum(results['n_events_tot'], np.ceil(RATIO_BY_TOTAL * results['n_events_tot']))
     
-    assert(len(results['n_events_target'] < 0) == 0)
+    assert(np.sum(results['n_events_target'] < 0) == 0)
     
     return results
 