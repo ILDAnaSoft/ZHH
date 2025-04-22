Fixed Normalization and AnalysisFinal workflow

workflows/analysis/tasks.py
@@ -82,7 +82,7 @@ class CreateAnalysisChunks(BaseTask):
         config = zhh_configs.get(str(self.tag))
         
         SAMPLE_INDEX = self.input()[0][1].path
-        DATA_ROOT = osp.dirname(self.input()[1]['collection'][0].path)
+        DATA_ROOT = osp.dirname(self.input()[1]['collection'][0][0].path)
         
         processes = np.load(self.input()[0][0].path)
         samples = np.load(SAMPLE_INDEX)

zhh/analysis/Normalization.py
@@ -91,6 +91,7 @@ def get_sample_chunk_splits(
         ('location', '<U512'),
         
         ('n_chunks', 'I'),
+        ('n_chunk_in_sample', 'I'),
         ('n_chunks_in_sample', 'I'),
         ('chunk_start', 'I'),
         ('chunk_size', 'I'),
@@ -142,12 +143,12 @@ def get_sample_chunk_splits(
             else:
                 n_accounted = 0
                 n_chunks = 0
-                n_chunks_in_sample = 0
                 n_sample = 0
             
             while n_sample < len(c_samples) and n_accounted < n_target:
                 sample = c_samples[n_sample]
                 
+                n_chunks_in_sample = 0
                 n_accounted_sample = 0
                 n_tot_sample = sample['n_events']
                 
@@ -159,21 +160,22 @@ def get_sample_chunk_splits(
                     
                 while n_accounted < n_target and n_accounted_sample < n_tot_sample:
                     c_chunk_size = min(min(n_tot_sample - n_accounted_sample, max_chunk_size), n_target - n_accounted)
-                    c_chunks.append((n_chunks_tot, sample['sid'], p['process'], p['proc_pol'], sample['location'], n_chunks, n_chunks_in_sample, n_accounted_sample, c_chunk_size))
+                    c_chunks.append((n_chunks_tot, sample['sid'], p['process'], p['proc_pol'], sample['location'], n_chunks, n_chunks_in_sample, 0, n_accounted_sample, c_chunk_size))
                     
                     n_accounted += c_chunk_size
                     n_accounted_sample += c_chunk_size
     
                     n_chunks += 1
+                    n_chunks_in_sample += 1
                     n_chunks_tot += 1
-                
-                for i in range(len(c_chunks)):
-                    c_chunks[i]['n_chunks_in_sample'] = n_chunks
                     
                 n_sample += 1
                     
-            if len(c_chunks) > 0:
-                results = np.append(results, np.array(c_chunks, dtype=dtype))
+                if len(c_chunks) > 0:
+                    results = np.append(results, np.array(c_chunks, dtype=dtype))
+                    results['n_chunks_in_sample'][results['sid'] == sample['sid']] = n_chunks_in_sample
+                    
+                    c_chunks.clear()
     
     return results
 